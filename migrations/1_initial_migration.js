b0683c3f7a6632d5cd603676c6f361565b0a65f3
gsofter <real.kingstar@outlook.com>
2021-08-12 09:13:16 -0500

    complete ZombieFactory

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
+const ZombieFactory = artifacts.require("ZombieFactory");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  deployer.deploy(ZombieFactory);
 };
