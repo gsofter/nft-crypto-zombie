4dd82b7e4bec83375cc7a3d1c16ceff3e352dc68
gsofter <real.kingstar@outlook.com>
2021-08-12 07:48:38 -0500

    add Zombie struct

--- contracts/ZombieFactory.sol
+++ contracts/ZombieFactory.sol
@@ -3,4 +3,9 @@ pragma solidity >=0.5.0 <0.9.0;
 contract ZombieFactory {
     uint256 dnaDigits = 16;
     uint256 danModulus = 10**dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint256 dna;
+    }
 }
